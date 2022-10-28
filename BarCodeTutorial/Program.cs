91ddb9b1deeba07de33a80da94c89049c066d685
kelia <kelia@KELIANSCHULE>
2022-10-28 15:17:31 +0200

    Projektdateien hinzufügen.

---
+++ BarCodeTutorial/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IronBarCode;
+
+namespace BarCodeTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var barcode = BarcodeWriter.CreateBarcode("First Barcode",BarcodeEncoding.Code128);
+            
+            barcode.SaveAsPng("Barcode.png");
+            barcode.AddBarcodeValueTextBelowBarcode();
+
+
+        }
+    }
+}
